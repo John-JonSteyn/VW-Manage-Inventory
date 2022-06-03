96f4b6ae0ec153735222d727fd0dc025b964e7e1
JohnJon-Steyn <johnjonsteyn@tutanota.com>
2022-06-03 09:24:39 +0200

    Database Tables Created

---
+++ BranchInv/dbo/Tables/ModelDetails.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[ModelDetails]
+(
+    [Model] TEXT NOT NULL, 
+    [Features] TEXT NULL, 
+    [Price] TEXT NULL, 
+    CONSTRAINT [FK_ModelDetails_ModelList] FOREIGN KEY ([Model]) REFERENCES [ModelList]([Model]), 
+    PRIMARY KEY ([Model])
+)
---
+++ BranchInv/dbo/Tables/ModelList.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[ModelList]
+(
+    [Make] TEXT NULL, 
+    [Model] TEXT NOT NULL, 
+    PRIMARY KEY ([Model])
+)
